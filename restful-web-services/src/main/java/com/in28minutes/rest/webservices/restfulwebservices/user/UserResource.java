99b43f89400d72eb0abcdfd8f0476f6509cae269
USERSAD\bmiguelr <bruno.miguel.ribeiro@emeal.nttdata.com>
2023-02-06 18:03:40 +0000

    Implementing Validations for REST API

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class UserResource {
 
@@ -47,7 +49,7 @@ public class UserResource {
 
 	// POST /users
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.save(user);
 
 		// /users/4 => /users/{id}, user.getId();

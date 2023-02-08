eee7f4bc3a3da1f1eca775778e077cfa634e6d92
USERSAD\bmiguelr <bruno.miguel.ribeiro@emeal.nttdata.com>
2023-02-08 10:03:12 +0000

    Creating User Entity and some test data

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details (id, birth_date, name) values (1001, current_date(), 'Bruno')
+insert into user_details (id, birth_date, name) values (1002, current_date(), 'Miguel')
+insert into user_details (id, birth_date, name) values (1003, current_date(), 'Pereira')
